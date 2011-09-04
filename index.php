15cd98ebcc1428ef434cf44cc029ee3aa189d2b9
bim <bim@192.168.1.100>
2011-09-04 18:22:03 +0200

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Testing</title>
     </head>
     <body>
         <?php
